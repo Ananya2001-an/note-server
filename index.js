2e2da42ae48ee0a6225f6e04d2111bc33a3f8187
Ananya Nayak <ananyanayak102@gmail.com>
2023-09-25 12:52:15 +0530

    create initDB file

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Note = require("./models/notes");
 const Assign = require("./models/assignments");
+require("dotenv").config();
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "*",
   })
 );
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
 
-db.on("error", (error) => {
-  console.error(error);
-});
-db.once("open", () => {
-  console.log("Connected to Database");
-});
+require('./initDB')();
 
 app.get("/", async (req, res) => {
   let assignments;
