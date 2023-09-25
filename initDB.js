cd9daca0ae9523c440a17a2ca165b153fa389997
Ananya Nayak <ananyanayak102@gmail.com>
2023-09-25 18:54:28 +0530

    Update initDB.js

--- initDB.js
+++ initDB.js
@@ -6,7 +6,6 @@ module.exports = () => {
     .connect(process.env.MONGODB_URI, {
       dbName: process.env.DB_NAME,
       user: process.env.DB_USER,
-      pass: process.env.DB_PASS,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
