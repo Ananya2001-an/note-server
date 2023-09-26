c28b6e853dbf272c8e81b98031dcc41ed96fa915
Ananya Nayak <ananyanayak102@gmail.com>
2023-09-26 23:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Note = require("./models/notes");
 const Assign = require("./models/assignments");
 require("dotenv").config();
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
